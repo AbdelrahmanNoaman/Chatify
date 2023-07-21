51b8c9cb9e939e6e9c8d88291b7f911d70779599
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2023-07-21 17:52:22 +0300

    Sending the message to and from users successfully

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,31 @@
+const chatForm = document.getElementById("chat-form");
+
 const socket = io();
 
+// Receiving the message from the server
 socket.on("message", (message) => {
   console.log(message);
+  outputMessage(message);
+  //Everytime we get a message we want to scroll down
 });
+
+//Adding a message
+chatForm = document.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //Getting the text of the message
+  const msg = e.target.elements.msg.value;
+  //Sending the msg to the server
+  socket.emit("chatMessage", msg);
+});
+
+//Adding the message to the client side
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `
+      <div class="message">
+        <p class="meta">Brad <span>9:12pm</span></p>
+        <p class="text">${message}</p>
+      </div>`;
+  document.querySelector(".chat-messages").appendChild(div);
+}
--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 const express = require("express");
-const http=require('http');
-
+const http = require("http");
 
 const dotenv = require("dotenv");
-const path=require('path');
-const socketio = require('socket.io');
+const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
-const server=http.createServer(app);
-const io = require('socket.io')(server)
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 
 dotenv.config();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-io.on('connection', socket =>{
-  console.log('New WS Connection....');
-  socket.emit('message','Welcome to Chatify');
-})
+io.on("connection", (socket) => {
+  //A single client
+  socket.emit("message", "Welcome to Chatify");
+  //Now we need to broadcast a message to everyone detecting that someone has joined the room
+  //It broadcasts the message to everyone except the one connected
+  socket.broadcast.emit("message", "A user has joined the chat");
+
+  socket.on("disconnect", () => {
+    //It broadcasts the message to everyone
+    io.emit("message", "A user has left the chat");
+  });
+
+  // Getting the chat message
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", msg);
+  });
+});
 
 server.listen(process.env.DEV_PORT || 3000, () => {
   console.log(`Connected on port ${process.env.DEV_PORT}`);
